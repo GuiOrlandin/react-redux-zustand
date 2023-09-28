8a60a44cc99913ab4b37a4f4e8447e56b0010c6f
Guilherme <guiorlandin@gmail.com>
2023-09-28 18:43:18 -0300

    loading implemented

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,13 @@
+import { useAppSelector } from "../store";
 import { useCurrentLesson } from "../store/slice/Player";
 
 export function Header() {
   const { currentModule, currentLesson } = useCurrentLesson();
+  const isCourseLoading = useAppSelector((state) => state.player.isLoading);
+
+  if (isCourseLoading) {
+    return <h1 className="text-2xl font-bold">Carregando</h1>;
+  }
 
   return (
     <div className="flex flex-col gap-1">
